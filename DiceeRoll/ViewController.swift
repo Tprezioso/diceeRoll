30a2eb69e0c9ed1860509a56039f00227a0c8000
Tprezioso <tommyprezioso@gmail.com>
2018-11-06 20:09:54 -0500

    first commit

---
+++ DiceeRoll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiceeRoll
+//
+//  Created by Thomas Prezioso on 11/6/18.
+//  Copyright © 2018 Thomas Prezioso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
